850e371c2f6e96de654f2248d137d2a2c0eab7dc
AngellilyK <angelinak572003d@mail.ru>
2023-02-28 02:30:58 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -35,11 +35,33 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**********': ' '
 };
 
 function decode(expr) {
-    // write your solution here
+    let array = expr.split('**********');
+
+    let el= array.map((x)=> {
+        let decodeText = '';
+        for (let i = 0; i < x.length / 10; i++) {
+            let el = '';
+            for (let j = i * 10; j < i * 10 + 10; j += 2) {
+                if (x[j] + x[j + 1] === '10') {
+                    el += '.';
+                } else if (x[j] + x[j + 1] === '11') {
+                    el += '-';
+                }
+            }
+
+            decodeText += MORSE_TABLE[el];
+            el = '';
+        }
+        return decodeText;
+    });
+
+    return el.join(' ');
 }
+
 
 module.exports = {
     decode
